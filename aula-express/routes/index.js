a7d6133366c71515399c4f0c792047aec7e4b4a9
jarbas coltro <jarbascltr@gmail.com>
2015-10-25 00:59:03 -0200

    Estudando o Código - Agora vai

--- aula-express/routes/index.js
+++ aula-express/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WebMob UNOESC' });
+  res.render('index', { title: 'Workshop Be MEAN' });
 });
 
 module.exports = router;
